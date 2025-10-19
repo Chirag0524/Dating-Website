c87777aced031628341f00e85cae7f216c7a74a5
Chirag Adwani <chiragadwani00@gmail.com>
2025-10-19 11:49:17 +0530

    Port changes

--- backend/server.js
+++ backend/server.js
@@ -69,5 +69,5 @@ app.post("/send-mail", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 2208;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
